9710c911f94e49b580a93fbee76ebd77d28a0e81
Mingos <137617609+Myngos@users.noreply.github.com>
2024-10-04 20:40:02 +0200

    reverse string tests done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    const revstring = str.split("").reverse().join("");
+    return revstring;
+    /*
+    The split() method divides the string into an array of characters, reverse() reverses the array, and 
+    join() combines the reversed characters into a new string, effectively reversing the original string.
+     */
 };
 
 // Do not edit below this line
