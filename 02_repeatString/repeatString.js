c8450b705746016955d2e40e583b5234df8531d6
Mingos <137617609+Myngos@users.noreply.github.com>
2024-10-04 02:24:33 +0200

    wtf

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
+let string = "hey"
+const repeatString = function(num) {
 
+    for(let i = 0; i <= num; i++){
+        console.log(string)
+    }
 };
 
 // Do not edit below this line
