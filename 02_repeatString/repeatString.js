8a086bf7c37d3937723bae26c492826d3edaed0a
Mingos <137617609+Myngos@users.noreply.github.com>
2024-10-04 19:02:23 +0200

    Test Driven Development

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,14 @@
-let string = "hey"
-const repeatString = function(num) {
 
-    for(let i = 0; i <= num; i++){
-        console.log(string)
+const repeatString = (word, num) => {
+
+    let string = "";
+    if(num < 0){
+        return "ERROR"
     }
+    for(let i = 0; i < num; i++){
+        string += word;
+    }
+    return string
 };
 
 // Do not edit below this line
