338448e06f242afffe8e8548bd079e91fd62f247
Mingos <137617609+Myngos@users.noreply.github.com>
2024-10-06 02:56:47 +0200

    am stuck

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, removeItem) {
+    const arr = [1, 2, 3,4]
+    return arr.filter(function(item) {
+        // Use strict comparison to avoid removing items of different types
+        a
+        return !removeItem.includes(item);
+    });
 };
 
 // Do not edit below this line
