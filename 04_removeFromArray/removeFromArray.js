8f81e4dbed81bc542cbb6e78347401abfe236e9e
Mingos <137617609+Myngos@users.noreply.github.com>
2024-10-09 14:32:15 +0200

    Update removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,7 +2,9 @@ const removeFromArray = function(arr, removeItem) {
     const arr = [1, 2, 3,4]
     return arr.filter(function(item) {
         // Use strict comparison to avoid removing items of different types
-        a
+        for(let i = 0; i >= arr.length; i++){
+            
+        }
         return !removeItem.includes(item);
     });
 };
