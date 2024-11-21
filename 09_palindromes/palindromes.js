1f59681892a99ac4e03cfdf9aea75fc3b60e3666
Mingos <moniusdomingo065@gmail.com>
2024-11-22 01:31:28 +0200

    palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    let cleanedStr = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    let reverse = cleanedStr.split("").reverse().join("");
+
+    if(reverse === cleanedStr){
+        return true
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
