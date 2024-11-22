e16dbad9f58bede354b8e58067cf0a78247e04e1
Mingos <moniusdomingo065@gmail.com>
2024-11-22 18:36:39 +0200

    fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0){
+        return "OOPS"
+    }
 
+    if(typeof num === 'string'){
+        num = Number(num);
+    }
+    if(num === 0) return 0;
+    if(num === 1) return 1;
+
+    let prev = 0, prev2 =1;
+    let current;
+
+    for(let i=2;i<=num; i++){
+        current = prev + prev2;
+        prev = prev2;
+        prev2 = current;
+    }
+    return current
 };
 
 // Do not edit below this line
