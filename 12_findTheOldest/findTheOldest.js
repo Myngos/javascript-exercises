a8666801221d61c56b4e34a5d152aea12f6a7438
Mingos <moniusdomingo065@gmail.com>
2024-11-22 22:13:15 +0200

    FindTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+    const currentYear = new Date().getFullYear()
+
+    let oldestPerson = arr.reduce((oldest, person)=>{
+
+        const personAge = (person.yearOfDeath || currentYear ) - person.yearOfBirth;
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+
+        if(personAge > oldestAge){
+            return person;
+        }else{
+            return oldest;
+        }
+    })
+    return oldestPerson;
 };
 
 // Do not edit below this line
